9c67816185f35c8ed6098f2ded5beafb4fdd838f
Najam Ul Hassan <161830987+RANANAJAMULHASSAN@users.noreply.github.com>
2024-06-11 15:15:21 +0500

    Add files via upload

---
+++ My Portfolio/main.js
@@ -0,0 +1,49 @@
+function msg()
+{
+  window.open("images/Netflix-clone-full-image.png", "_blank");
+
+
+}
+function msg1()
+{
+window.open("images/Microsoft-clone full-img.png", "_blank");
+
+}
+function msg3()
+{
+window.open("images/Shopify-full-image.png", "_blank");
+
+}
+function msg4()
+{
+window.open("images/UltraEdit-full-image.png", "_blank");
+
+}
+function msg5()
+{
+window.open("https://github.com/RANANAJAMULHASSAN/Netflix-Clone", "_blank");
+
+}
+function msg6()
+{
+window.open("https://github.com/RANANAJAMULHASSAN/MicroSoft-Clone", "_blank");
+
+}
+function msg7()
+{
+window.open("https://github.com/RANANAJAMULHASSAN/Shopify-Clone", "_blank");
+
+}
+function msg8()
+{
+window.open("https://github.com/RANANAJAMULHASSAN/UltraEdit-Clone", "_blank");
+
+}
+
+
+
+// For resume
+const download = document.querySelector('#DOWNLOAD')
+download.addEventListener('click', function () {
+  window.opener('https://Resume/Najam-Ul-Hassan -CV.pdf','_blank')
+})
